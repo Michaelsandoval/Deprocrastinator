dda11dc00e4027b6f3832eef499d0e34bc722524
Steven Fellows <stevenfellows@Stevens-MacBook-Pro-2.local>
2016-01-18 14:11:38 -0600

    second commit

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *toDoTextField;
 
 @end
 
@@ -16,12 +17,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (IBAction)onAddButtonPressed:(UIBarButtonItem *)sender
+{
+    
 }
 
 @end
