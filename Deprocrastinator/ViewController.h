dd64b81034eafeb75beae5fb00ad36fa3b30d2f9
Michael Sandoval <michaelsandoval@michaels-macbook-pro-2.local>
2016-01-18 13:57:06 -0600

    Initial Commit

---
+++ Deprocrastinator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Deprocrastinator
+//
+//  Created by Michael Sandoval on 1/18/16.
+//  Copyright © 2016 Michael Sandoval. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
